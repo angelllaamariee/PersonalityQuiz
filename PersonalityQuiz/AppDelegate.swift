9c928abdaf6e8c9096cbe203ffb6b70976a2e369
Angela Sanchez <60831893+angelllaamariee@users.noreply.github.com>
2022-03-13 16:36:15 -0400

    Add files via upload

---
+++ PersonalityQuiz/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  PersonalityQuiz
+//
+//  Created by Angela Sanhcez on 3/8/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    
+    }
+
+
+}
---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Angela Sanhcez on 3/8/22.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
